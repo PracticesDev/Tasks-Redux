f93144933e9203f3481e283a28b435b69bf01e8f
Jorge Avella <79177900+jaavella07@users.noreply.github.com>
2024-02-08 22:53:21 -0500

    Recibir data en el form

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,20 +2,38 @@ import { Link as RouterLink } from 'react-router-dom';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { Google } from '@mui/icons-material';
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hook';
 
 
 export const LoginPage = () => {
+
+  const { onInputChange, onResetForm,email,password} = useForm({
+    email:'jorge@google.com',
+    password:'123456'
+    
+  });
+
+  const onSubmit = (event ) => {
+    event.preventDefault();
+
+    console.log({email,password})
+  }
+
+
   return (
     <AuthLayout title="Login">
-      <form>
+      <form onSubmit={  onSubmit }>
           <Grid container>
             <Grid item xs={ 12 } sx={{ mt: 2 }}>
               <TextField 
                 label="Correo" 
                 type="email" 
                 placeholder='correo@google.com' 
+                name='email'
+                value={email}
+                onChange={onInputChange}
                 fullWidth
-              />
+                />
             </Grid>
 
             <Grid item xs={ 12 } sx={{ mt: 2 }}>
@@ -23,13 +41,16 @@ export const LoginPage = () => {
                 label="Contraseña" 
                 type="password" 
                 placeholder='Contraseña' 
+                name='password'
+                value={password}
+                onChange={onInputChange}
                 fullWidth
-              />
+                />
             </Grid>
             
             <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
               <Grid item xs={ 12 } sm={ 6 }>
-                <Button variant='contained' fullWidth>
+                <Button type='submit' variant='contained' fullWidth>
                   Login
                 </Button>
               </Grid>
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom';
 
 
 import './styles.css';
 import { TravelApp } from './TravelApp';
+import store from './store/Store';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <TravelApp/>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <TravelApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
